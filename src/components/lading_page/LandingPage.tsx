280a59b4646695295bfdbeefe36b2bd0a2eb6182
hashsham123 <hashshamkhan8@gmail.com>
2024-12-30 00:56:19 +0530

    New logic added

--- src/components/lading_page/LandingPage.tsx
+++ src/components/lading_page/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../../styles/LandingPage.scss";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -40,14 +40,67 @@ const frames = [
   "frames/scene00042.png",
 ];
 
+const frames2 = [
+  "frames2/scene00006.png",
+  "frames2/scene00007.png",
+  "frames2/scene00008.png",
+  "frames2/scene00009.png",
+  "frames2/scene00010.png",
+  "frames2/scene00011.png",
+  "frames2/scene00012.png",
+  "frames2/scene00013.png",
+  "frames2/scene00014.png",
+  "frames2/scene00015.png",
+  "frames2/scene00016.png",
+  "frames2/scene00017.png",
+  "frames2/scene00018.png",
+  "frames2/scene00019.png",
+  "frames2/scene00020.png",
+  "frames2/scene00021.png",
+  "frames2/scene00022.png",
+  "frames2/scene00023.png",
+  "frames2/scene00024.png",
+  "frames2/scene00025.png",
+  "frames2/scene00026.png",
+  "frames2/scene00027.png",
+  "frames2/scene00028.png",
+  "frames2/scene00029.png",
+  "frames2/scene00030.png",
+  "frames2/scene00031.png",
+  "frames2/scene00032.png",
+  "frames2/scene00033.png",
+  "frames2/scene00034.png",
+  "frames2/scene00035.png",
+  "frames2/scene00036.png",
+  "frames2/scene00037.png",
+  "frames2/scene00038.png",
+  "frames2/scene00039.png",
+  "frames2/scene00040.png",
+  "frames2/scene00041.png",
+  "frames2/scene00042.png",
+  "frames2/scene00043.png",
+  "frames2/scene00044.png",
+  "frames2/scene00045.png",
+  "frames2/scene00046.png",
+  "frames2/scene00047.png",
+  "frames2/scene00048.png",
+  "frames2/scene00049.png",
+  "frames2/scene00050.png",
+  "frames2/scene00051.png",
+  "frames2/scene00052.png",
+  "frames2/scene00053.png",
+];
+
 const LandingPage: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const imageCache = useRef<Map<number, HTMLImageElement>>(new Map());
   const animationFrame = useRef<number>(0);
+  const [activeFrames, setActiveFrames] = useState<string[]>(frames);
 
   // Preload images and store them in a cache
-  const preloadImages = () => {
-    frames.forEach((frame, index) => {
+  const preloadImages = (framesArray: string[]) => {
+    imageCache.current.clear(); // Clear previous cache
+    framesArray.forEach((frame, index) => {
       const img = new Image();
       img.src = `/${frame}`;
       img.onload = () => {
@@ -76,9 +129,25 @@ const LandingPage: React.FC = () => {
   };
 
   useEffect(() => {
-    preloadImages();
+    // Determine the correct frames array based on width
+    const updateFrames = () => {
+      const isSmallScreen = window.innerWidth <= 700;
+      setActiveFrames(isSmallScreen ? frames2 : frames);
+    };
 
-    const totalFrames = frames.length - 1;
+    updateFrames();
+    window.addEventListener("resize", updateFrames);
+
+    return () => {
+      window.removeEventListener("resize", updateFrames);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Preload images and set up animation whenever activeFrames changes
+    preloadImages(activeFrames);
+
+    const totalFrames = activeFrames.length - 1;
 
     gsap.to(animationFrame, {
       current: totalFrames,
@@ -101,7 +170,7 @@ const LandingPage: React.FC = () => {
     return () => {
       ScrollTrigger.killAll(); // Clean up ScrollTrigger on unmount
     };
-  }, []);
+  }, [activeFrames]);
 
   return (
     <div>
